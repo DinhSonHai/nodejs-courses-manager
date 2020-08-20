b6368f6543039bacb3db47caeb0a4c1b118a57c6
DinhSonHai <sdinhhai@gmail.com>
2020-08-20 12:27:47 +0700

    Split route in app.js to routes and controllers folder

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
+const route = require('./routes/index');
 
 //Với các file tĩnh sẽ đi theo đường dẫn này
 app.use(express.static(path.join(__dirname, 'public')));
@@ -28,22 +29,9 @@ app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources\\views'));
 // console.log(path.join(__dirname, 'resources\\views'));
 
-app.get('/', (req, res) => {
-  res.render('home');
-});
 
-app.get('/news', (req, res) => {
-  res.render('news');
-});
-
-app.get('/search', (req, res) => {
-  res.render('search');
-});
-
-app.post('/search', (req, res) => {
-  console.log(req.body);
-  res.send('');
-})
+//Routes init
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
