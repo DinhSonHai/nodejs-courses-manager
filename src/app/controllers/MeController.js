1bd928779afe78f29064375c78fcd9785e43258d
DinhSonHai <sdinhhai@gmail.com>
2020-08-21 22:30:17 +0700

    Add soft delete feature

--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -12,6 +12,17 @@ class MeController {
             })
             .catch(next);
     }
+
+    //[GET] /me/trash/courses
+    trashCourses(req, res, next) {
+        Courses.findDeleted({})
+            .then((courses) => {
+                res.render('me/trash-courses', {
+                    courses: multipleMongooseToObject(courses),
+                });
+            })
+            .catch(next);
+    }
 }
 
 module.exports = new MeController();
--- src/routes/me.js
+++ src/routes/me.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const meControllers = require('../app/controllers/MeController');
 
 router.get('/stored/courses', meControllers.storedCourses);
+router.get('/trash/courses', meControllers.trashCourses);
 // router.get('/', newsControllers.index);
 
 module.exports = router;
