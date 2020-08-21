9588e4954e1718d8d222955876273e3beb35fdf8
DinhSonHai <sdinhhai@gmail.com>
2020-08-21 20:24:04 +0700

    Add delete course feature

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -43,8 +43,16 @@ class CourseController {
 
     //[PUT] /courses/:id
     update(req, res, next) {
+        req.body.image = `https://img.youtube.com/vi/${req.body.videoId}/mqdefault.jpg`;
         Course.updateOne({ _id: req.params.id }, req.body)
-            .then((course) => res.redirect('/me/stored/courses'))
+            .then(() => res.redirect('/me/stored/courses'))
+            .catch(next);
+    }
+
+    //[DELETE] /courses/:id
+    delete(req, res, next) {
+        Course.deleteOne({ _id: req.params.id })
+            .then(() => res.redirect('back'))
             .catch(next);
     }
 }
--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -4,7 +4,6 @@ const { multipleMongooseToObject } = require('../../util/mongoose');
 class SiteController {
     //[GET]
     index(req, res, next) {
-        console.log(multipleMongooseToObject);
         Courses.find({})
             .then((courses) => {
                 res.render('home', {
