55eed509d0f405252dec0b02804df675900fb3a0
DinhSonHai <sdinhhai@gmail.com>
2020-09-07 14:33:27 +0700

    Add option connect with MongoDB Atlas

---
+++ src/app/models/User.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const slug = require('mongoose-slug-generator');
+const mongooseDelete = require('mongoose-delete');
+
+const Schema = mongoose.Schema;
+
+const User = new Schema(
+    {
+        name: { type: String, required: true },
+    },
+    {
+        timestamps: true,
+    },
+);
+
+mongoose.plugin(slug);
+User.plugin(mongooseDelete, {
+    deletedAt: true,
+    overrideMethods: 'all',
+});
+
+module.exports = mongoose.model('User', User);
--- src/config/db/index.js
+++ src/config/db/index.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 
 async function connect() {
     try {
+        //process.env.MONGO_ATLAS_URL
+        //process.env.MONGO_URL
         await mongoose.connect(process.env.MONGO_URL, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
