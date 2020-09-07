d99810dd535d780631344da17e996c722e80b147
DinhSonHai <sdinhhai@gmail.com>
2020-09-07 12:21:26 +0700

    Apply sort for multiple column

--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -4,13 +4,13 @@ const { multipleMongooseToObject } = require('../../util/mongoose');
 class MeController {
     //[GET] /me/stored/courses
     storedCourses(req, res, next) {
-        res.json(res.locals._sort);
+        // res.json(res.locals._sort);
         let courseQuery = Courses.find({});
-        // if (req.query.hasOwnProperty('_sort')) {
-        //     courseQuery = courseQuery.sort({
-        //         [req.query.column]: req.query.type,
-        //     });
-        // }
+        if (req.query.hasOwnProperty('_sort')) {
+            courseQuery = courseQuery.sort({
+                [req.query.column]: req.query.type,
+            });
+        }
         Promise.all([courseQuery, Courses.countDocumentsDeleted()])
             .then(([courses, deletedCount]) =>
                 res.render('me/stored-courses', {
